48ba84f6110b646ae984f27873460fe3eda4c076
Александр <aBekar145@gmail.com>
2020-08-20 20:51:01 +0300

    added project Angeek

---
+++ public/assets/scripts/scripts.js
@@ -0,0 +1,18 @@
+
+//C:\Projects\angeek\src\assets\scripts\tools.js
+//-->
+jQuery(document).ready(function($) {
+	
+    $('a.js-anchor-go').click(function(){
+        $('html, body').animate({
+                scrollTop: $( $(this).attr('href') ).offset().top
+            }, 800, 'swing');
+        return false;
+    });
+
+	$( ".services__tabs-wrap" ).tabs({
+        classes: { "ui-tabs-active": "services-tabs__item--active" },
+		hide: { effect: "fade", duration: 200 },
+		show: { effect: "fade", duration: 200 }
+	});
+});
